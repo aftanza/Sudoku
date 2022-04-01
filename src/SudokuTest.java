05472c11847bc8dc501c9b6fc286953c51d7683e
Zaza <zazaprabowo@gmail.com>
2022-04-02 00:18:09 +0800

    better modified

--- src/SudokuTest.java
+++ src/SudokuTest.java
@@ -15,18 +15,18 @@ public class SudokuTest {
     }
 
     /*Check row/col/3x3 box for conflicting numbers*/
-    public boolean checkRow(int row, int numToCheck){
+    public boolean checkRow(int row, int col, int numToCheck){
         for(int i=0; i<9; ++i){
-            if(i != select[1]) {
+            if(i != col) {
                 if (arr[row][i][0] == numToCheck)
                     return true;
             }
         }
         return false;
     }
-    public boolean checkCol(int col, int numToCheck){
+    public boolean checkCol(int row, int col, int numToCheck){
         for(int i=0; i<9; ++i){
-            if(i != select[0]) {
+            if(i != row) {
                 if (arr[i][col][0] == numToCheck)
                     return true;
             }
@@ -97,7 +97,7 @@ public class SudokuTest {
     public void printTable(){
         for(int i=0; i<rowLength; ++i){
             for(int j=0; j<colLength; ++j){
-                System.out.printf("%3d ", arr[i][j][0]);
+                    System.out.printf("%3d ", arr[i][j][0]);
                 if((j+1)%3 == 0)
                     System.out.print("| ");
                 if(j == colLength-1)
@@ -127,7 +127,7 @@ public class SudokuTest {
             if(arr[randRow][randCol][0] != 0) {
                 --i;
             }
-            else if( (checkCol(randCol, randNum)) || (checkRow(randRow, randNum)) || checkBox(randRow, randCol, randNum) ){
+            else if( (checkCol(randRow, randCol, randNum)) || (checkRow(randRow, randCol, randNum)) || checkBox(randRow, randCol, randNum) ){
                 --i;
             }
             else {
@@ -174,7 +174,7 @@ public class SudokuTest {
                         select[1] = j;
                         randNum = rng.nextInt(9) + 1;
 
-                        if( (checkCol(j, randNum)) || (checkRow(i, randNum)) || checkBox(i, j, randNum) ){
+                        if( (checkCol(i, j, randNum)) || (checkRow(i, j, randNum)) || checkBox(i, j, randNum) ){
                             --i;
                         }
                         else {
@@ -300,8 +300,6 @@ public class SudokuTest {
 //            }
 //            catch (InterruptedException e){
 //            }
-            printTable();
-
             if (currentSelectedState() == 0) {
                 ++arr[select[0]][select[1]][0];
                 isReversing = false;
@@ -325,7 +323,7 @@ public class SudokuTest {
 
             isReversing = true;
             for (int i = currentSelectedNum(); i <= 9; ++i) {
-                if (!(checkRow(select[0], i)) && !(checkCol(select[1], i)) && !(checkBox(select[0], select[1], i))) {
+                if (!(checkRow(select[0], select[1], i)) && !(checkCol(select[0], select[1], i)) && !(checkBox(select[0], select[1], i))) {
                     arr[select[0]][select[1]][0] = i;
                     isReversing = false;
                     break;
@@ -369,44 +367,44 @@ public class SudokuTest {
 
     public static void main(String[] args) {
         SudokuTest game = new SudokuTest();
+
         game.resetTable();
-        game.setRandomPermanents(30);
+//        game.printTable();
+//        game.setRandomPermanents(17);
+//        game.setRandomDiagonal();
+        game.setRandomX();
+        game.solveTable();
+        game.printTable();
+        game.setFirstSolution();
+        game.setAllPermanent();
+
+        for(int i=1; i<=81; ++i){
+            game.setChangeableToZero();
+            game.removeRandomPermanent(1);
+//            game.printTable();
+
+            game.solveTable();
+//            game.printTable();
+
+            game.findOtherSolution();
+//            game.printTable();
+
+            if(!game.compareWithFirstSolution()){
+                if(!game.areThereZeroes()){
+                    game.setChangeableToZero();
+                    game.solveTable();
+                    if(!game.compareWithFirstSolution()){
+                        game.setCacheAsCurrent();
+//                      game.printTable();
+                        System.out.println((81-i+1));
+                        break;
+                    }
+                }
+            }
+        }
+
         game.printTable();
         game.solveTable();
-//        game.printTable();
-
-
-//        game.resetTable();
-////        game.printTable();
-////        game.setRandomPermanents(17);
-////        game.setRandomDiagonal();
-//        game.setRandomX();
-//        game.solveTable();
-//        game.printTable();
-//        game.setFirstSolution();
-//        game.setAllPermanent();
-//
-//        for(int i=1; i<=81; ++i){
-//            game.setChangeableToZero();
-//            game.removeRandomPermanent(1);
-////            game.printTable();
-//
-//            game.solveTable();
-////            game.printTable();
-//
-//            game.findOtherSolution();
-////            game.printTable();
-//
-//            if(!game.compareWithFirstSolution()){
-//                if(!game.areThereZeroes()){
-//                    game.setCacheAsCurrent();
-////                    game.printTable();
-//                    System.out.println((81-i+1));
-//                    break;
-//                }
-//            }
-//        }
-//
-//        game.printTable();
+        game.printTable();
     }
 }
